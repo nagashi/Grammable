d6cbe76a93fa4cd163bfddda0c2d230126affe27
Charles ORiley <ceoriley@gmail.com>
2019-06-06 01:05:17 -0500

    Add Ability to Destroy Grams

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -2,26 +2,41 @@ require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
 
-  describe "grams#update action" do
-    it "should allow users to successfully update grams" do
-      gram = FactoryBot.create(:gram, message: "Initial Value")
+  describe 'grams#destroy action' do
+    it 'should allow a user to destroy grams' do
+      gram = FactoryBot.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it 'should return a 404 message if we cannot find a gram with the id that is specified' do
+      delete :destroy, params: { id: 'SPACEDUCK' }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
+  describe 'grams#update action' do
+    it 'should allow users to successfully update grams' do
+      gram = FactoryBot.create(:gram, message: 'Initial Value')
       patch :update, params: { id: gram.id, gram: { message: 'Changed' } }
       expect(response).to redirect_to root_path
       gram.reload
-      expect(gram.message).to eq "Changed"
+      expect(gram.message).to eq 'Changed'
     end
 
-    it "should have http 404 error if the gram cannot be found" do
-      patch :update, params: { id: "YOLOSWAG", gram: { message: 'Changed' } }
+    it 'should have http 404 error if the gram cannot be found' do
+      patch :update, params: { id: 'YOLOSWAG', gram: { message: 'Changed' } }
       expect(response).to have_http_status(:not_found)
     end
 
-    it "should render the edit form with an http status of unprocessable_entity" do
-      gram = FactoryBot.create(:gram, message: "Initial Value")
+    it 'should render the edit form with an http status of unprocessable_entity' do
+      gram = FactoryBot.create(:gram, message: 'Initial Value')
       patch :update, params: { id: gram.id, gram: { message: '' } }
       expect(response).to have_http_status(:unprocessable_entity)
       gram.reload
-      expect(gram.message).to eq "Initial Value"
+      expect(gram.message).to eq 'Initial Value'
     end
   end
   
@@ -59,7 +74,7 @@ RSpec.describe GramsController, type: :controller do
     end
 
     describe 'grams#new action' do
-      it "should require users to be logged in" do
+      it 'should require users to be logged in' do
         get :new
         expect(response).to redirect_to new_user_session_path    
       end
@@ -74,7 +89,7 @@ RSpec.describe GramsController, type: :controller do
 
     describe 'grams#create action' do
       it 'should require users to be logged in' do
-        post :create, params: { gram: { message: "Hello" } }
+        post :create, params: { gram: { message: 'Hello' } }
         expect(response).to redirect_to new_user_session_path
       end
 
